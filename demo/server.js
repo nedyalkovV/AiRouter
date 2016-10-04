95d7450e85ac7146784c4ce79a1e98d7021e7d1d
Vasil Nedyalkov <vasil@gidr.io>
2016-10-04 15:19:02 +0300

    test added, bux fix in group routing

--- demo/server.js
+++ demo/server.js
@@ -7,7 +7,9 @@ var app = express();
 const PORT = 8080;
 
 app.use('/demo', express.static(__dirname + '/demo/'));
-
+app.use('/', (req, res)=>{
+    res.redirect('/demo/#/');
+});
 //Lets start our server
 app.listen(PORT, function(){
     console.log("Server listening on: http://localhost:%s", PORT);
--- tests/server.js
+++ tests/server.js
@@ -4,9 +4,12 @@ var http = require('http');
 var fs = require('fs');
 var express = require('express');
 var app = express();
-const PORT = 8000;
+const PORT = 8080;
 
 app.use('/tests', express.static(__dirname + '/tests/'));
+app.use('/', (req, res)=>{
+    res.redirect('/tests/#/');
+});
 
 //Lets start our server
 app.listen(PORT, function(){
